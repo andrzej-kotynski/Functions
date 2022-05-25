bbbe95810c5cb6fe9a3626bba2dcf3c5956bf60c
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-05-25 05:57:09 +0200

    Add project files.

---
+++ Functions/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myInt = InvokeTwoPlusOne(2);
+
+            var userInput1 = Console.ReadLine();
+            //Console.WriteLine(userInput1);
+
+            var list = new List<int>();
+
+            list.Add(myInt);
+
+            Console.WriteLine(list[0]);
+
+        }
+
+        static int InvokeTwo()
+        {
+            return 2;
+        }
+
+        static int InvokeTwoPlusOne(int myInt)
+        {
+            return myInt + 1;
+        }
+    }
+}
